9642c5a13695b120ab6ebd6eb24664c1d1217881
Jerico de Guzman <rickzx98@gmail.com>
2016-06-06 23:24:33 +0800

    updates

---
+++ src/public/fsd-app/fsd-app.bdd.ts
@@ -0,0 +1,3 @@
+describe("Create donor BDD", () => {
+
+});
--- src/public/fsd-app/fsd-map/fsd-sidebar/fsd-donor-form/fsd-donor-form.component.ts
+++ src/public/fsd-app/fsd-map/fsd-sidebar/fsd-donor-form/fsd-donor-form.component.ts
@@ -3,7 +3,7 @@ import {Point} from "esri";
 @Component({
   selector: "fsd-donor-form",
   template: `
-    <form novalidate #donorForm="ngForm" *ngIf="active" role="form" (ngSubmit)="onSubmit()" class="container-fluid">
+    <form #donorForm="ngForm" *ngIf="active" role="form" (ngSubmit)="onSubmit()" class="container-fluid">
       <div class="form-group">
         <label class="col-lg-6 col-md-6 col-sm-12 col-xs-12 control-label">
           Firstname
